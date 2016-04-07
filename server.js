ea0411b9c2c9fa9e25e0393d7c2e43f738fa4eb2
Chris Pauley <chrispauley92@gmail.com>
2016-04-07 07:16:31 -0500

    reworked SQL query

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var server = http.createServer((req, res) => {
   if (page < 0) {
     db.each("SELECT timestamp, name, response FROM records WHERE `name`=? ORDER BY timestamp DESC", name, iterateFunction, completeFunction);
   } else {
-    db.each("SELECT timestamp, name, response FROM records WHERE `name`=? LIMIT ? OFFSET ? ORDER BY timestamp DESC", name, limit, offset, iterateFunction, completeFunction);
+    db.each("SELECT timestamp, name, response FROM records WHERE `name`=?  ORDER BY timestamp DESC LIMIT ? OFFSET ?", name, limit, offset, iterateFunction, completeFunction);
   }
   
   function iterateFunction (err,row) {
